3e825e0b96db17d1ae4596514d0b7c901d350b95
Brandon Fulljames <bfullj@gmail.com>
2018-10-20 23:30:05 -0700

    Number tweaks

--- projects/sample/AbilityValues.h
+++ projects/sample/AbilityValues.h
@@ -3,8 +3,8 @@
 namespace AbilityValues {
 	namespace Pyromancer {
 		const float IgniteCooldown = 5.f;
-		const float FirestreamChargeSeconds = 1.f;
-		const float FirestreamRechargeRate = 0.5f;
+		const float FirestreamChargeSeconds = 3.f;
+		const float FirestreamRechargeRate = 0.4f;
 
 		// TODO: Handle this over lower frame rates better
 		const float FirestreamCooldown = 0.02f;
